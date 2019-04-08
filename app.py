941e2cfd23db413c20f988170817c047e2e88a6c
ch264 <christinavhastenrath@gmail.com>
2019-04-08 15:49:50 -0700

    heroku

--- app.py
+++ app.py
@@ -83,10 +83,10 @@ def after_request(response):
 def not_found_error(error):
     return render_template('404.html'), 404
 
-@app.errorhandler(500)
-def internal_error(error):
-    g.db.session.rollback()
-    return render_template('500.html'), 500
+# @app.errorhandler(500)
+# def internal_error(error):
+#     g.db.session.rollback()
+#     return render_template('500.html'), 500
 
 
 # ====================================================================
