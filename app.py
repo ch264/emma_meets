29c761828c789df605e1be0a4720c4e16ae03137
ch264 <christinavhastenrath@gmail.com>
2019-04-11 15:37:48 -0700

    cleanup app.py

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 import os
 from flask import Flask, g, request, render_template, flash, redirect, url_for, session, escape
+# refractor code and use Config file
 # from config import Config
 
 # User login
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 from flask_bcrypt import check_password_hash, Bcrypt, generate_password_hash
 import models, forms
+
 # Redirect user when not logged in
 from werkzeug.urls import url_parse
 
 # email confirmation
 from itsdangerous import URLSafeTimedSerializer
-# ts = URLSafeTimedSerializer(app.secret_key)
 from flask_mail import Mail, Message
 
 # Image uploader
@@ -23,10 +24,10 @@ from werkzeug import secure_filename
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('flask.cfg')
 app.secret_key = 'pafajeihguihawiorhgl'
-# config for email sending
+
+# config for sending emails, should go to config file
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 465
-# app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'lalatestingemma@gmail.com'
 app.config['MAIL_PASSWORD'] = 'Mysanfran3'
 app.config['MAIL_USE_TLS'] = False
@@ -40,14 +41,10 @@ mail = Mail(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
-# ts = URLSafeTimedSerializer(app.secret_key)
 
 # Sets variable images to uploader
 images = UploadSet('images', IMAGES)
 configure_uploads(app, images)
-
-
-
 
 
 @login_manager.user_loader
@@ -95,20 +92,6 @@ def internal_error(error):
 		# copied to routes
 @app.route('/')
 def index():
-  def send_reset_email(user):
-    token = user.get_reset_token()
-    # msg = Message('Hello', sender = 'lalatestingemma@gmail.com', recipients = ['id1@gmail.com'])
-    # msg.body = "This is the email body"
-    # mail.send(msg)
-    # return "Sent"
-    msg = Message('Password Reset Request', sender='lalatestingemma@gmail.com', recipients =[user.email])
-    msg.body = f''' 
-    
-    To reset your password, visit the following link: {url_for('reset_token', token=token, _external=True)} 
-    
-    If you did not make this request then simply ignore this email'''
-    mail.send(msg)
-    return "Sent"
   return render_template('landing.html')
 
 @app.route('/about')
@@ -118,6 +101,12 @@ def about():
 
 @app.route('/contact-us')
 def contact_us():
+  # Future email send to me through contacat form
+  # def send_reset_email(user):
+  #   msg = Message('Contact Request', sender='lalatestingemma@gmail.com', recipients =[lalatestingemma@gmail.com])
+  #   msg.body = form.body.data
+  #   mail.send(msg)
+  #   return "Sent"
 	return render_template('contact-us.html')
 
 
@@ -128,15 +117,12 @@ def contact_us():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = forms.SignUpForm()
-    print('out if create signup')
-    print(request.form)
     if form.validate_on_submit():
       # Sets variable filename to image file of uploaded 'profile_image' from form
       filename = images.save(request.files['profile_image'])
       # Sets variable url to change image url to match filename
       url = images.url(filename)
-      # Calls method 'create_user' as defined in models.py to create a user in database
-      print('in if create user')
+
       models.User.create_user(
         username = form.username.data,
         email = form.email.data,
@@ -153,14 +139,13 @@ def signup():
         
       # Gets newly created user from the database by matching username in the database to username entered in the form
       user = models.User.get(models.User.username == form.username.data)
-        # Creates logged in session
+      # Creates logged in session
       login_user(user)
       flash('Thank you for signing up', 'success')
-        # Pass in current/logged in user as parameter to method 'profile' in order to redirect user to profile after signing up
       return redirect(url_for('profile', username=user.username))
     return render_template('signup.html', form=form)
 
-# Route and method to login
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   form = forms.LoginForm()
@@ -176,15 +161,12 @@ def login():
         # Creates logged in session
         login_user(user)
         flash("You successfully logged in", "success")
-        # Upon successful login, redirect to user's profile page with user passed in as a parameter to the method 'profile'
         return redirect(url_for('profile', username=user.username))
       else:
-        # If passwords don't match, flash error message
         flash("Your email or password doesn't match", "error")
-  # Initial visit to this page renders the login template with the LoginForm passed into it
   return render_template('login.html', form=form)
 
-# Route and method to logout
+
 @app.route('/logout')
 @login_required
 def logout():
@@ -196,29 +178,30 @@ def logout():
 # ====================================================================
 # =========================  Profile Routes  =========================
 # ====================================================================
-# Route and method to go to a user's profile
+
 @app.route('/profile/<username>', methods=['GET', 'DELETE'])
 @login_required
 def profile(username=None):
   if username != None:
     # Finds user in database by username passed into URL
     user = models.User.select().where(models.User.username==username).get()
-    # Finds all reviews in database where the user id stored with a reciew matches the found user aboves id
+    # Finds all reviews in database where the user id stored with a review matches the found user above id
     reviews = models.Review.select().where(models.Review.user == user.id).order_by(-models.Review.timestamp)
     
     saved_product = models.Saved.select(models.Saved, models.Product).join(models.Product).where(models.Saved.user==user.id, models.Saved.product==models.Product.id)
+
+    #Future Feature : users can follow each other
     # //////////////treehouse ///////////
     # finds all followers and followed
     # following = models.Follow.select().where(models.Follow)
-
-
+    
     return render_template('profile.html', user=user, reviews=reviews, saved_product=saved_product)
   return redirect(url_for('index'))
 
 def send_reset_email(user):
   token = user.get_reset_token()
   msg = Message('Password Reset Request', sender='lalatestingemma@gmail.com', recipients =[user.email])
-  msg.body = f''' To reset your password, visit the following link: {url_for('reset_token', token=token, _external=True)} If you did not make this request then simply ignore this email'''
+  msg.body = f''' To reset your Emma Meets password, visit the following link: {url_for('reset_token', token=token, _external=True)} If you did not make this request then simply ignore this email'''
   mail.send(msg)
   return "Sent"
 
@@ -234,8 +217,6 @@ def edit_profile(username=None):
     print('in if edit user')
     user.username = form.username.data
     user.email = form.email.data
-    # user.password = generate_password_hash(form.password.data)
-    # user.password = form.password.data
     user.about_me = form.about_me.data
     user.age = form.age.data
     user.gender = form.gender.data
@@ -247,43 +228,40 @@ def edit_profile(username=None):
     user.save()
     flash('Your changes have been saved.', 'success')
     return redirect(url_for('profile', username=user.username))
-  # form.breed.default = user.breed
-  # form.process()
   return render_template('edit-profile.html', form=form, user=user)
 
-# ====================================================================
-# =========================  Follow Routes  =========================
-# ====================================================================
+# ==================================================================================
+# =========================  Follow Routes  FUTURE FEATURE =========================
+# ==================================================================================
 
 # //////////////// MegaFlask Tutorial //////////////////
+# @app.route('/follow/<username>')
+# @login_required
+# def follow(username):
+#   user = User.query.filter_by(username=username).first()
+#   if user is None:
+#     return redirect(url_for('index'))
+#   if user == current_user:
+#     flash('You cannot follow yourself!')
+#     return redirect(url_for('profile', username=username))
+#   current_user.follow(user)
+#   flash('You are following {}!'.format(username))
+#   return redirect(url_for('profile', username=username))
 
-@app.route('/follow/<username>')
-@login_required
-def follow(username):
-  user = User.query.filter_by(username=username).first()
-  if user is None:
-    return redirect(url_for('index'))
-  if user == current_user:
-    flash('You cannot follow yourself!')
-    return redirect(url_for('profile', username=username))
-  current_user.follow(user)
-  flash('You are following {}!'.format(username))
-  return redirect(url_for('profile', username=username))
-
-@app.route('/unfollow/<username>')
-@login_required
-def unfollow(username):
-  user = User.query.filter_by(username=username).first()
-  if user is None:
-    flash('User {} not found.'.format(username))
-    return redirect(url_for('index'))
-  if user == current_user:
-    flash('You cannot unfollow yourself!')
-    return redirect(url_for('user', username=username))
-  current_user.unfollow(user)
-  db.session.commit()
-  flash('You are not following {}.'.format(username))
-  return redirect(url_for('user', username=username))
+# @app.route('/unfollow/<username>')
+# @login_required
+# def unfollow(username):
+#   user = User.query.filter_by(username=username).first()
+#   if user is None:
+#     flash('User {} not found.'.format(username))
+#     return redirect(url_for('index'))
+#   if user == current_user:
+#     flash('You cannot unfollow yourself!')
+#     return redirect(url_for('user', username=username))
+#   current_user.unfollow(user)
+#   db.session.commit()
+#   flash('You are not following {}.'.format(username))
+#   return redirect(url_for('user', username=username))
 
 # //////////////// Treehouse tutorial ////////////////////////////
 # @app.route('/follow/<username>')
@@ -304,7 +282,6 @@ def unfollow(username):
 # def unfollow(username):
 #     try:
 #       user = models.User.get(models.User.id == username)
-      
 #       models.Follow.get(
 #         models.Follow.follower == g.user._get_current_object(),
 #         models.Follow.followed == user
@@ -312,7 +289,6 @@ def unfollow(username):
 #     except models.DoesNotExist:
 #       pass
 #     return render_template('profile.html', user=user.username)
-  
 
 # ====================================================================
 # =========================  Product Routes  =========================
@@ -335,7 +311,6 @@ def product(product_id=None):
     return render_template('product.html', product=product, reviews=reviews, rating=rating)
   # if no product_is is provided as a parameter, select all product form the product table, limit 15 results
   products = models.Product.select().limit(15)
-  # pass those products to the products template
   return render_template('products.html', products=products)
 
 @app.route('/create-product', methods=['GET', 'POST'])
@@ -365,7 +340,6 @@ def add_product():
   if form.validate_on_submit():
     filename = images.save(request.files['product_image'])
     url = images.url(filename)
-
     prod = models.Product.create_product(
       name = form.name.data,
       location = form.location.data,
@@ -374,10 +348,6 @@ def add_product():
       image_filename = filename,
       image_url = url)
     product = models.Product.get(models.Product.website == form.website.data)
-    
-    # return render_template('create-product.html', form=form, user=user)
-    # Find new created product in database
-    
     flash('Product Created', 'Success')
     return redirect(url_for('product', product_id=product.id))
   return render_template('create-product.html', form=form, user=user, categories=categories)
@@ -412,8 +382,8 @@ def edit_product(product_id=None):
       product.save()
       flash('Your edited Product is now online', 'success')
       return redirect(url_for('product', product_id=product.id))
-    # form.process()
     return render_template('edit-product.html', product=product, form=form, user=user)
+
 # ====================================================================
 # =========================  Review Routes  =========================
 # ====================================================================
@@ -421,13 +391,9 @@ def edit_product(product_id=None):
 @app.route('/review/<product_id>', methods=['GET', 'POST'])
 @login_required
 def add_review(product_id):
-  
   form = forms.ReviewForm()
-  print('out if')
-  print(request.form)
   product = models.Product.select().where(models.Product.id == product_id).get()
   if form.validate_on_submit():
-    print(form.rating.data)
     models.Review.create_review(
       title = form.title.data,
       user = g.user._get_current_object(),
@@ -479,10 +445,8 @@ def edit_review(review_id=None):
 @app.route('/save/<product_id>')
 def save_to_profile(product_id=None):
   user = g.user._get_current_object()
-  # user = models.User.get(g.user.id)
   if product_id != None:
     product = models.Product.get(models.Product.id == product_id)
-    print(user.id)
     models.Saved.create(
       user=user.id, 
       product=product_id)
@@ -493,12 +457,8 @@ def save_to_profile(product_id=None):
 @app.route('/remove/<product_id>', methods=['GET', 'DELETE'])
 @login_required
 def remove_saved(product_id=None):
-  # user = models.User.get(g.user.id)
   user = models.User.get(g.user.id)
-  # user = g.user._get_current_object()
-  print('out if')
   if product_id != None:
-    print(product_id)
     remove_saved = models.Saved.delete().where(models.Saved.user_id == user.id, models.Saved.product_id == product_id)
     remove_saved.execute()
     return redirect(url_for('profile', username=user.username))
@@ -517,12 +477,8 @@ def reset_request():
   if current_user.is_authenticated:
     return redirect(url_for('index'))
   form = forms.RequestResetForm()
-
   if form.validate_on_submit():
-    print('in if')
-    # user = models.User.query.filter_by(email=form.email.data).first()
     user = models.User.get(models.User.email == form.email.data)
-    # if user:
     send_reset_email(user)
     flash('An email has been sent to reset password', 'info')
     return redirect(url_for('login'))
@@ -536,15 +492,10 @@ def reset_token(token):
     return redirect(url_for('index'))
   user = models.User.verify_reset_token(token)
   if not user:
-    print('invalid token')
-  # if user is None:
-    # pass in class warningk, can be used by css
     flash('That is an invalid or expired token', 'warning')
     return redirect(url_for('reset_request'))
   form = forms.ResetPasswordForm()
-  print('show form reset password')
   if form.validate_on_submit():
-    print('in hashed password')
     hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
     user.password = hashed_password
     user.save()
@@ -554,51 +505,7 @@ def reset_token(token):
 
 
 
-
-
-
-# use salt when generating a token
-
-
-# @app.route('/reset', methods=["GET", "POST"])
-# def reset():
-#   form = forms.EmailForm()
-#   if form.validate_on_submit():
-#     user = User.query.filter_by(email=form.email.data)
-#     token = ts.dumps(self.email, salt='recover-key')
-
-#     recover_url = url_for('reset_with _token', token=token, _external=True)
-
-#     html = render_template('email-recover.html', recover_url=recover_url)
-# # Let's assume that send_email was defined in myapp/util.py
-#     send_email(user.email, subject, html)
-#     return redirect(url_for('index'))
-#   return render_template('reset.html', form=form)
-
-# @app.route('/reset/<token>', methods=['GET', 'POST'])
-# def reset_with_token(token):
-#   try:
-#     email = ts.loads(token, salt="recover-key", max_age=86400)
-#   except:
-#     abort(404)
-
-#   form = PasswordForm()
-
-#   if form.validate_on_submit():
-#     user = User.query.filter_by(email=email).first_or_404()
-
-#     user.password = form.password.data
-
-#     db.session.add(user)
-#     db.session.commit()
-
-#     return redirect(url_for('signin'))
-
-#   return render_template('reset_with_token.html', form=form, token=token)
-
-
-
-
+# needed for further development
 PORT = 5000
 DEBUG = True
 
@@ -611,14 +518,9 @@ if 'ON_HEROKU' in os.environ:
 if __name__ == '__main__':
 	models.initialize()
 
-app.run(port=PORT, debug=DEBUG)
-	# try:
-	# 	models.Category.create_category(
-	# 		name='name')
-	# 		except ValueError:
-	# 			pass
 
-  # app.config['DEBUG'] = True
-  
+# needed for further development
+app.run(port=PORT, debug=DEBUG)
+
 
 
